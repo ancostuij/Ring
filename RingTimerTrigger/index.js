bf84ec4ca957e6b04771cd42191742014716ef31
ancostuij <22628756+ancostuij@users.noreply.github.com>
2018-12-30 22:51:59 +0100

    Read secrets from keyvault

--- RingTimerTrigger/index.js
+++ RingTimerTrigger/index.js
@@ -1,3 +1,6 @@
+const RingUserName = process.env['RingUserName'];
+const RingPassword = process.env['RingPassword'];
+
 module.exports = async function (context, myTimer) {
     var timeStamp = new Date().toISOString();
     
@@ -5,5 +8,6 @@ module.exports = async function (context, myTimer) {
     {
         context.log('JavaScript is running late!');
     }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    context.log('JavaScript timer trigger function ran!', timeStamp);
+    context.log(RingUserName, RingPassword); 
 };
